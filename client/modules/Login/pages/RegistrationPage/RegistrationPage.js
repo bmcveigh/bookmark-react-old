42c68227f0ec2447618052f4e8b4597caa47e2c0
Brian McVeigh <bmcveigh@forumone.com>
2019-03-24 14:58:45 -0400

    Registration page component submit handler.

--- client/modules/Login/pages/RegistrationPage/RegistrationPage.js
+++ client/modules/Login/pages/RegistrationPage/RegistrationPage.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import { injectIntl, intlShape } from 'react-intl';
 import styles from '../LoginPage/LoginPage.css';
 
-class RegistrationPage extends Component {
+export class RegistrationPage extends Component {
+
+  handleFormSubmit = () => {
+    const usernameRef = this.refs.username;
+    const emailRef = this.refs.email;
+    const passRef = this.refs.pass;
+
+    if (usernameRef && emailRef && passRef) {
+      // todo: register the user.
+    }
+  };
+
   render() {
     return (
       <div>
         <input ref="username" className={styles['form-field']} placeholder={this.props.intl.messages.username} />
         <input placeholder={"Email"} className={styles['form-field']} ref="email" />
         <input type="password" placeholder={this.props.intl.messages.username} ref="pass" className={styles['form-field']} />
-        <button>Submit</button>
+        <button className={styles['post-submit-button']} onClick={this.handleFormSubmit}>Submit</button>
       </div>
     );
   }
+
 }
 
 RegistrationPage.propTypes = {
