4c252cd25ef9b4ddc95526476517af13804fc0cb
Brian McVeigh <bmcveigh@forumone.com>
2019-03-27 12:25:59 -0400

    Adding support for spanish.

---
+++ Intl/localizationData/es.js
@@ -0,0 +1,33 @@
+export default {
+  locale: 'es',
+  messages: {
+    siteTitle: 'Marcador Gurú',
+    addPost: 'Ajouter Poster',
+    switchLanguage: 'Cambiar idioma',
+    twitterMessage: 'Somos en Twitter',
+    by: 'Par',
+    deletePost: 'Supprimer le message',
+    createNewPost: 'Créer un nouveau message',
+    authorName: 'Nom de l\'auteur',
+    username: 'Nombre de usuario',
+    pass: 'Contraseña',
+    postTitle: 'Titre de l\'article',
+    postContent: 'Contenu après',
+    submit: 'Soumettre',
+    comment: `user {name} {value, plural,
+    	  =0 {does not have any comments}
+    	  =1 {has # comment}
+    	  other {has # comments}
+    	} (in real app this would be translated to French)`,
+    HTMLComment: `user <b style='font-weight: bold'>{name} </b> {value, plural,
+    	  =0 {does not have <i style='font-style: italic'>any</i> comments}
+    	  =1 {has <i style='font-style: italic'>#</i> comment}
+    	  other {has <i style='font-style: italic'>#</i> comments}
+    	} (in real app this would be translated to French)`,
+    nestedDateComment: `user {name} {value, plural,
+  		  =0 {does not have any comments}
+  		  =1 {has # comment}
+  		  other {has # comments}
+  		} as of {date} (in real app this would be translated to French)`,
+  },
+};
