aa64de92e9a1491f342e75b72702b6a1bc805e68
Brian McVeigh <bmcveigh@forumone.com>
2019-03-31 14:00:42 -0400

    Theme settings modification

---
+++ client/components/forms/UserThemeSettingsForm/UserThemeSettingsForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class UserThemeSettingsForm extends React.Component {
+
+  render() {
+    return (
+      <form>
+        <h3>Theme Settings</h3>
+      </form>
+    );
+  }
+
+}
+
+export default UserThemeSettingsForm;
--- client/components/pages/user/UserThemeSettingsPage/UserThemeSettingsPage.js
+++ client/components/pages/user/UserThemeSettingsPage/UserThemeSettingsPage.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import MenuSidebarPage from '../../../../modules/Bookmark/pages/MenuSidebarPage/MenuSidebarPage';
+import UserThemeSettingsForm from '../../../forms/UserThemeSettingsForm/UserThemeSettingsForm';
 
 function UserThemeSettingsPage() {
   return (
     <div>
       <MenuSidebarPage>
-        <div>
-          Test
-        </div>
+        <UserThemeSettingsForm />
       </MenuSidebarPage>
     </div>
   );
