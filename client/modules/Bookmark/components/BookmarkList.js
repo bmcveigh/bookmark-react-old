cf40088ea30d78d76347e106bda96d6e23bf61c7
Brian McVeigh <bmcveigh@forumone.com>
2019-04-01 15:27:49 -0400

    Added tabs for bookmark spaces.

--- client/modules/Bookmark/components/BookmarkList.js
+++ client/modules/Bookmark/components/BookmarkList.js
@@ -2,15 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import BookmarkAddCategoryButton from './BookmarkAddCategoryButton/BookmarkAddCategoryButton';
 import BookmarkCategories from './BookmarkCategories/BookmarkCategories';
+import Tabs from '../../../components/elements/sierra/Tabs/Tabs';
 
 function BookmarkList(props) {
   if (!props.user.bookmarkSpaces) {
     return '';
   }
 
+  const bkSpaceTabsData = props.user.bookmarkSpaces[0].map((space) => {
+    return {
+      label: space.name,
+      href: window.location.pathname,
+    };
+  });
+
   return (
     <div>
       <BookmarkAddCategoryButton />
+      <Tabs data={bkSpaceTabsData} />
       {
         props.user.bookmarkSpaces.map((bookmarkSpace, key) => {
           const space = bookmarkSpace[0];
