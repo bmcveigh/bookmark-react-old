00a1777232d0f1761a499bf53626822935321990
Brian McVeigh <bmcveigh@forumone.com>
2019-04-05 13:32:21 -0400

    Providing users with ability to customize their theme.

--- client/components/containers/MenuSidebarContainer/MenuSidebarContainer.js
+++ client/components/containers/MenuSidebarContainer/MenuSidebarContainer.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import radium from 'radium';
 
 import SidebarMenu from '../../../components/elements/SidebarMenu/SidebarMenu';
 
 import classes from './MenuSidebarContainer.css';
+import { getUserPreferenceStyles } from '../../../store/actions/globalStylesActions';
 import { getTabData } from '../../../store/actions/tabDataActions';
 
 class MenuSidebarContainer extends Component {
@@ -13,15 +15,26 @@ class MenuSidebarContainer extends Component {
   constructor(props, context) {
     super(props, context);
     this.props.dispatch(getTabData());
+    this.props.dispatch(getUserPreferenceStyles(props.user));
   }
 
   render() {
+    let output;
+
+    if (this.props.userPreferenceStyles) {
+      output = (
+        <div className={classes.PageContentsWrapper} style={this.props.userPreferenceStyles.background}>
+          <div className={classes.PageContents}>
+            {this.props.children}
+          </div>
+        </div>
+      );
+    }
+
     return (
       <div>
         <SidebarMenu />
-        <div className={classes.PageContents}>
-          {this.props.children}
-        </div>
+        {output}
       </div>
     );
   }
@@ -31,10 +44,14 @@ MenuSidebarContainer.propTypes = {
   children: PropTypes.any,
   dispatch: PropTypes.func,
   tabData: PropTypes.object,
+  user: PropTypes.object,
+  userPreferenceStyles: PropTypes.object,
 };
 
 function mapStateToProps(state) {
   return {
+    user: state.user,
+    userPreferenceStyles: state.styles.userPreferenceStyles,
     tabData: state.tabData.data,
   };
 }
@@ -42,8 +59,9 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
     dispatch,
+    getUserPreferenceStyles,
     getTabData,
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(MenuSidebarContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(radium(MenuSidebarContainer));
--- client/store/actions/globalStylesActions.js
+++ client/store/actions/globalStylesActions.js
@@ -1,11 +1,29 @@
 // Export Constants
 export const SET_GLOBAL_STYLES = 'SET_GLOBAL_STYLES';
+export const GET_USER_PREFERENCE_STYLES = 'GET_USER_PREFERENCE_STYLES';
 
 export function setGlobalStyles(styles) {
   return (dispatch) => {
     return dispatch({
       type: SET_GLOBAL_STYLES,
       data: styles,
+    });
+  };
+}
+
+export function getUserPreferenceStyles(user) {
+  return (dispatch) => {
+    const preferences = user.preferences;
+    const themeSettings = preferences.themeSettings;
+    const colors = themeSettings.colors.split(/,/g);
+
+    return dispatch({
+      type: GET_USER_PREFERENCE_STYLES,
+      data: {
+        background: {
+          background: colors[0],
+        },
+      },
     });
   };
 }
