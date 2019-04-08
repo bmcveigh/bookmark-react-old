c3d10918ec129b6db13ef0b5f810e5f27b8488ea
Brian McVeigh <bmcveigh@forumone.com>
2019-04-08 15:02:26 -0400

    Do not delete the main bookmark space.

--- client/modules/Bookmark/components/BookmarkList.js
+++ client/modules/Bookmark/components/BookmarkList.js
@@ -9,6 +9,15 @@ import BookmarkAddCategoryButton from './BookmarkAddCategoryButton/BookmarkAddCa
 import BookmarkCategories from './BookmarkCategories/BookmarkCategories';
 import Tabs from '../../../components/elements/Tabs/Tabs';
 import AppModal from '../../../components/containers/AppModal/AppModal';
+import { updateUserById } from '../../../store/actions/UserActions';
+
+const confirmHandler = (props, bookmarkSpaces, index) => {
+  const user = props.user;
+
+  // todo: add logic for confirm handler.
+  bookmarkSpaces.splice(index);
+  props.dispatch(updateUserById(user._id, user));
+};
 
 function BookmarkList(props) {
   const bkSpaceTabsData = props.user.bookmarkSpaces[0].map((space, index) => {
@@ -20,6 +29,8 @@ function BookmarkList(props) {
 
   const params = props.params || {};
   const index = params.id || 0;
+  const user = props.user || false;
+  const bookmarkSpaces = user.bookmarkSpaces[0];
 
   return (
     <div>
@@ -27,32 +38,30 @@ function BookmarkList(props) {
       <BookmarkAddSpaceForm />
       <Tabs data={bkSpaceTabsData} />
       {
-        props.user.bookmarkSpaces.map((bookmarkSpace, key) => {
-          return (
-            <div key={key}>
-              <Row className="float-right">
-                <Col
-                  md={12}
-                >
-                  <AppModal
-                    labelId="deleteThisSpace"
-                    confirmHandler={null}
-                  >
-                    <span>Are you sure you would like to delete this space?</span>
-                  </AppModal>
-                </Col>
-              </Row>
-              <BookmarkCategories space={bookmarkSpace[index]} />
-            </div>
-          );
-        })
+        index > 0 ? (
+          <Row className="float-right">
+            <Col
+              md={12}
+            >
+              <AppModal
+                labelId="deleteThisSpace"
+                confirmHandler={() => confirmHandler(props, bookmarkSpaces, index)}
+              >
+                <span>Are you sure you would like to delete this space?</span>
+              </AppModal>
+            </Col>
+          </Row>
+        ) : null
       }
+      <BookmarkCategories space={bookmarkSpaces[index]} />
     </div>
   );
 }
 
 BookmarkList.propTypes = {
+  dispatch: PropTypes.func,
   globalStyles: PropTypes.object,
+  params: PropTypes.object,
   user: PropTypes.any, // .isRequired,
 };
 
@@ -63,4 +72,11 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookmarkList);
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+    updateUserById,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookmarkList);
