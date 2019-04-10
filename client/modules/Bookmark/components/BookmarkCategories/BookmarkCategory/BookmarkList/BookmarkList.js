1b2c398651cc16774277e286460c412140297d25
Brian McVeigh <bmcveigh@forumone.com>
2019-04-10 16:02:28 -0400

    Provide the ability to delete bookmarks

--- client/modules/Bookmark/components/BookmarkCategories/BookmarkCategory/BookmarkList/BookmarkList.js
+++ client/modules/Bookmark/components/BookmarkCategories/BookmarkCategory/BookmarkList/BookmarkList.js
@@ -3,29 +3,64 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import radium from 'radium';
 
-import PropTypes from 'prop-types';
-
-import { SortableContainer as sc, SortableElement as se } from 'react-sortable-hoc';
 import arrayMove from 'array-move';
 
-const SortableItem = se(radium(({ index, value, userPreferenceStyles }) => (
-  <li style={userPreferenceStyles.cardLink}>
-    <a
-      href={value.href}
-      key={index}
-      target="_blank"
-      style={userPreferenceStyles.cardLink}
-    >
-      {value.label}
-    </a>
-  </li>
-)));
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
 
-const SortableList = sc(({ items, userPreferenceStyles }) => {
+import PropTypes from 'prop-types';
+
+import classes from './BookmarkList.css';
+
+import { SortableContainer as sc, SortableElement as se } from 'react-sortable-hoc';
+import { updateUserById } from '../../../../../../store/actions/UserActions';
+
+const handleDeleteBookmarkClick = (e, bookmark, props) => {
+  e.preventDefault();
+  const index = props.routeParams.id ? props.routeParams.id : 0;
+  const user = props.user;
+  const space = user.bookmarkSpaces[0][index];
+  const categories = space.bookmarkCategories;
+
+  categories.forEach(category => {
+    category.bookmarks.forEach((bm, i) => {
+      if (
+        bm.bookmarkId === bookmark.bookmarkId
+      ) {
+        category.bookmarks.splice(i, 1);
+      }
+    });
+  });
+  props.dispatch(updateUserById(user._id, user));
+};
+
+const SortableItem = se(
+  radium(({ index, value, props }) => (
+    <li style={props.userPreferenceStyles.cardLink}>
+      <a
+        href={value.href}
+        key={index}
+        target="_blank"
+        style={props.userPreferenceStyles.cardLink}
+      >
+        {value.label}
+      </a>
+      <a
+        href="#"
+        className={`${classes.DeleteBookmark} float-right`}
+        onClick={(e) => handleDeleteBookmarkClick(e, value, props)}
+      >
+        <FontAwesomeIcon icon={faWindowClose} />
+      </a>
+    </li>
+  ),
+));
+
+const SortableList = sc(({ items, props }) => {
   return (
     <ul>
       {items.map((value, index) => (
-        <SortableItem key={`item-${index}`} index={index} value={value} userPreferenceStyles={userPreferenceStyles} />
+        <SortableItem key={`item-${index}`} index={index} value={value} props={props} />
       ))}
     </ul>
   );
@@ -50,7 +85,7 @@ class BookmarkList extends Component {
 
   shouldCancelStart(e) {
     // Cancel sorting if the event target is an anchor tag (`a`)
-    if (e.target.tagName.toLowerCase() === 'a') {
+    if (e.target.tagName.toLowerCase() === 'a' || e.target.tagName.toLowerCase() === 'svg') {
       return true; // Return true to cancel sorting
     }
     return false;
@@ -61,7 +96,7 @@ class BookmarkList extends Component {
       <SortableList
         items={this.props.bookmarks}
         onSortEnd={this.onSortEnd}
-        userPreferenceStyles={this.props.userPreferenceStyles}
+        props={this.props}
         shouldCancelStart={this.shouldCancelStart}
         style={this.props.userPreferenceStyles}
       />
@@ -75,6 +110,8 @@ BookmarkList.propTypes = {
 
 function mapStateToProps(state) {
   return {
+    routeParams: state.routeParams,
+    user: state.user,
     userPreferenceStyles: state.styles.userPreferenceStyles,
   };
 }
