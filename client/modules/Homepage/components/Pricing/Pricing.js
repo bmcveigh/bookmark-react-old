3a013391241468395857454183ff071dc6b23f92
Brian McVeigh <bmcveigh@forumone.com>
2019-04-30 14:38:45 -0400

    Data for plans

--- client/modules/Homepage/components/Pricing/Pricing.js
+++ client/modules/Homepage/components/Pricing/Pricing.js
@@ -5,29 +5,65 @@ import { Card, Col, Jumbotron, Row } from 'reactstrap';
 import classes from './Pricing.css';
 
 function Pricing() {
+  const plans = {
+    availablePlans: [
+      {
+        name: 'Basic',
+        price: '0.00',
+        featuresIncluded: {
+          createMultipleBookmarkSpaces: null,
+        },
+      },
+      {
+        name: 'Premium',
+        price: '0.99',
+        featuresIncluded: {
+          createMultipleBookmarkSpaces: null,
+          customizeAppearance: null,
+        },
+      },
+      {
+        name: 'Enterprise',
+        price: '1.99',
+        featuresIncluded: {
+          createMultipleBookmarkSpaces: null,
+          customizeAppearance: null,
+        },
+      },
+    ],
+    availableFeatures: {
+      createMultipleBookmarkSpaces: 'Create multiple bookmark spaces',
+      customizeAppearance: 'Customize appearance',
+    },
+  };
+
   return (
     <Jumbotron className={classes.Pricing}>
       <h1>Pricing</h1>
       <p>Below are the plans that we offer.</p>
       <Row>
-        <Col md={4}>
-          <Card className={classes.Card}>
-            <h3>Basic</h3>
-            <span>$0.00 /month</span>
-          </Card>
-        </Col>
-        <Col md={4}>
-          <Card className={classes.Card}>
-            <h3>Premium</h3>
-            <span>$0.99 /month</span>
-          </Card>
-        </Col>
-        <Col md={4}>
-          <Card className={classes.Card}>
-            <h3>Enterprise</h3>
-            <span>$1.99 /month</span>
-          </Card>
-        </Col>
+        {
+          plans.availablePlans.map((plan, index) => (
+            <Col md={4} key={index}>
+              <Card className={classes.Card}>
+                <h3>{plan.name}</h3>
+                <span>${plan.price} /month</span>
+                <div className={classes.Features}>
+                  {
+                    Object.keys(plan.featuresIncluded).map((featureKey, fIndex) => (
+                      <div
+                        key={fIndex}
+                        className={classes.Feature}
+                      >
+                        {plans.availableFeatures[featureKey]}
+                      </div>
+                    ))
+                  }
+                </div>
+              </Card>
+            </Col>
+          ))
+        }
       </Row>
     </Jumbotron>
   );
