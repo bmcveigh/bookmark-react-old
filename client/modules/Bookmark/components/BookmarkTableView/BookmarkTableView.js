da0c72bcbdd88bbe02579e19eceb0a100d22f35f
Brian McVeigh <bmcveigh@forumone.com>
2019-05-08 12:21:00 -0400

    Styling for the bookmark table view

--- client/modules/Bookmark/components/BookmarkTableView/BookmarkTableView.js
+++ client/modules/Bookmark/components/BookmarkTableView/BookmarkTableView.js
@@ -3,52 +3,58 @@ import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 
-import { Table } from 'reactstrap';
+import { Col, Row, Table } from 'reactstrap';
 
 import classes from './BookmarkTableView.css';
 
 function BookmarkTableView(props) {
+  const userPreferenceStyles = props.userPreferenceStyles || {};
+
   return (
-    <Table className={classes.BookmarkTableView}>
-      <thead>
-        <tr>
-          <th>Bookmark</th>
-          <th>Bookmark Category</th>
-        </tr>
-      </thead>
-      <tbody>
-      {
-        props.space.bookmarkCategories.map(category => {
-          return category.bookmarks.map((bookmark, key) => (
-            <tr key={key}>
-              <td>
-                <a
-                  href={bookmark.href}
-                  target="_blank"
-                >
-                  {bookmark.label}
-                </a>
-              </td>
-              <td>{category.name}</td>
+    <Row>
+      <Col md={12} className={classes.BookmarkTableView}>
+        <Table>
+          <thead>
+            <tr style={userPreferenceStyles.cardHeading}>
+              <th>Bookmark</th>
+              <th>Bookmark Category</th>
             </tr>
-          ));
-        })
-      }
-      </tbody>
-    </Table>
+          </thead>
+          <tbody>
+          {
+            props.space.bookmarkCategories.map(category => {
+              return category.bookmarks.map((bookmark, key) => (
+                <tr key={key}>
+                  <td>
+                    <a
+                      href={bookmark.href}
+                      target="_blank"
+                    >
+                      {bookmark.label}
+                    </a>
+                  </td>
+                  <td>{category.name}</td>
+                </tr>
+              ));
+            })
+          }
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
   );
 }
 
 BookmarkTableView.propTypes = {
   space: PropTypes.object.isRequired,
-  styles: PropTypes.object,
+  userPreferenceStyles: PropTypes.object,
 };
 
 // Retrieve data from store as props
 function mapStateToProps(state) {
   return {
-    styles: state.styles.data,
     user: state.user,
+    userPreferenceStyles: state.userPreferenceStyles,
   };
 }
 
