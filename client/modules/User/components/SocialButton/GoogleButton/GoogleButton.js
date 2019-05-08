ca55011d06bacb5a4acbfea169d1dc2f845b2ee4
Brian McVeigh <bmcveigh@forumone.com>
2019-05-08 15:15:34 -0400

    Include email address in scope

--- client/modules/User/components/SocialButton/GoogleButton/GoogleButton.js
+++ client/modules/User/components/SocialButton/GoogleButton/GoogleButton.js
@@ -2,11 +2,11 @@ import React from 'react';
 import SocialButton from '../SocialButton';
 
 const handleSocialLogin = (user) => {
-  // console.log(user);
+  console.log(user);
 };
 
 const handleSocialLoginFailure = (err) => {
-  // console.error(err);
+  console.error(err);
 };
 
 const GoogleButton = () => (
@@ -15,7 +15,7 @@ const GoogleButton = () => (
     appId="51928739653-qc1a21ee2ak22jl7ip7gh2t0o21vkej5.apps.googleusercontent.com"
     onLoginSuccess={handleSocialLogin}
     onLoginFailure={handleSocialLoginFailure}
-    scope="profile"
+    scope="profile email"
   >
     Login with Google
   </SocialButton>
