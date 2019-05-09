aaca1363be6064cd998d4443eaf5947498a3f7b1
Brian McVeigh <bmcveigh@forumone.com>
2019-05-09 11:22:17 -0400

    Fixed an issue with space toggling

--- client/routes.js
+++ client/routes.js
@@ -45,15 +45,7 @@ export default (
       path="/space/:id"
       getComponent={(nextState, cb) => {
         require.ensure([], require => {
-          const isUserLoggedIn = typeof localStorage !== 'undefined' && localStorage.getItem('token') || false;
-
-          if (isUserLoggedIn) {
-            cb(null, require('./modules/Bookmark/pages/BookmarkListPage/BookmarkListPage').default);
-          }
-          // Fix ESLint issue.
-          if (!isUserLoggedIn) {
-            cb(null, require('./modules/User/pages/LoginPage/LoginPage').default);
-          }
+          cb(null, require('./modules/Bookmark/pages/BookmarkListPage/BookmarkListPage').default);
         });
       }}
     />
